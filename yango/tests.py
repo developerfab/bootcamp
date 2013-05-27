7dd97993f0bb5f74540ca23dcef201cb1cdad7c7
root <root@CRISTIAN-PC.fabricio.com>
2013-05-27 16:30:48 -0500

    Adiciona pruebas para la funcion fibonnaci
    Este commit envia las pruebas que se realizaron el dia de hoy en django

--- yango/tests.py
+++ yango/tests.py
@@ -7,10 +7,29 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
+def fibo(x):
+    if x==1 or x==2:
+        return x
+    return fibo(x-1)+fibo(x-2)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class FibonacciTest(TestCase):
+    def test_fibo_de_1_es_1(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests que prueba que fibo de 1 sea 1.
         """
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(fibo(1), 1)
+
+    def test_fibo_de_2_es_2(self):
+	"""
+	Test que prueba que fibo de 2 sea 2
+	"""
+	self.assertEqual(fibo(2),2)
+
+    def test_fibo_de_3_es_3(self):
+	"""
+	Test que prueba que fibo de 3 sea 3
+	"""
+	self.asserEqual(fibo(3),3)
+
+    def test_fibo_n_es_la_suma_de_los_2_anterioes
+	self.asserEqual(fibo(10),89)
