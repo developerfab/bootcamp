e23c8d00bcc981bf80fa18e0f2b13f023f94df87
root <root@CRISTIAN-PC.fabricio.com>
2013-05-28 07:25:31 -0500

    Test de numero primo
    Este es el test del numero primo

--- yango/tests.py
+++ yango/tests.py
@@ -7,26 +7,11 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
-def multiplos(lim):
-    x=1
-    sum=0
-    mult3=0
-    mult5=0
-    while mult3<lim:
-        sum=sum+mult3
-        mult3=3*x
-        x=x+1
-    x=1
-    while mult5<lim:
-        sum=sum+mult5
-        mult5=5*x
-        x=x+1
-    print sum
-    return sum
+class Primo(TestCase):
 
-class Multiplos(TestCase):
-    def test_suma_multiplos_3_y_5_de_10_es_23(self):
+    def test_primo_de_4_es_2(self):
 	"""
-	Test que prueba que la suma de las multiplos de 3 y 5 menores que 10 es 23
+	Test que comprueba 
 	"""
-	self.assertEqual(multiplos(10),23)
+	num=primo(4)
+        self.assertEqual(num[2],True)
