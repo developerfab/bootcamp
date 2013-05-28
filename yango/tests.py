1cfde9579fe83b40aa8de2b689bc0d74a7f87b13
root <root@CRISTIAN-PC.fabricio.com>
2013-05-28 08:21:31 -0500

    se recarga multiplicar

--- yango/tests.py
+++ yango/tests.py
@@ -6,6 +6,12 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+"""
+def primo(x):
+    if x==1:
+        return 1
+    else:
+        
 
 class Primo(TestCase):
 
@@ -15,3 +21,29 @@ class Primo(TestCase):
 	"""
 	num=primo(4)
         self.assertEqual(num[2],True)
+"""
+from django.test import TestCase
+
+def multiplos(lim):
+    x=1
+    sum=0
+    mult3=0
+    mult5=0
+    while mult3<lim:
+        sum=sum+mult3
+        mult3=3*x
+        x=x+1
+    x=1
+    while mult5<lim:
+        sum=sum+mult5
+        mult5=5*x
+        x=x+1
+    print sum
+    return sum
+
+class Multiplos(TestCase):
+    def test_suma_multiplos_3_y_5_de_10_es_23(self):
+	"""
+	Test que prueba que la suma de las multiplos de 3 y 5 menores que 10 es 23
+	"""
+	self.assertEqual(multiplos(10),23)
